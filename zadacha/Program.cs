e052e3a0666c43c0ec1e48c2b605738cc45d2f2a
Daria <82046@mail.ru>
2022-12-06 16:04:19 +0300

    корректировка решения

--- zadacha/Program.cs
+++ zadacha/Program.cs
@@ -2,35 +2,7 @@
  длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
   либо задать на старте выполнения алгоритма. 
   При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
-
-
-
-int size = 3;
-string[] firstarray = new string [size];
-Console.WriteLine("Введите первоначальный массив");
-//Console.Clear();
-FillArray(firstarray);
-PrintArray(firstarray);
-
-void FillArray (string [] arr)
-{
-    for ( int i=0; i<size; i++)
-    {
-        arr[i]=Console.ReadLine() ?? "";
-    }
-
-}
-
-
-void PrintArray (string [] arr)
-{
-    Console.Write("[ ");
-    for ( int i=0; i<size; i++)
-    {
-        Console.Write(arr[i] + " ");
-    }
-    Console.Write("]");
-} */
+ */
 
 Console.Write("Сколько элементов Вы хотите ввести? ");
 int size = Convert.ToInt32(Console.ReadLine());
@@ -39,8 +11,8 @@ string[] arrayStrings = new string[size];
 for (int i = 0; i < size; i++)
 {
     Console.WriteLine($"Введите {i+1}-й элемент: ");
-    string element = Convert.ToString(Console.ReadLine());
-    arrayStrings[i] = element;
+    string arr = Convert.ToString(Console.ReadLine());
+    arrayStrings[i] = arr;
 }
 
 string[] arrayFinal = new string[size];
